57cad0f8071e139e75d2ea1d037ce3acf4016fcf
GarryChen <chenymj23@gmail.com>
2022-05-28 22:30:21 +0800

    linux OpenPoll

--- eventloop/internal/internal_linux.go
+++ eventloop/internal/internal_linux.go
@@ -6,3 +6,23 @@ type Poll struct {
 	wfd 	int	// wake  fd
 	notes 	noteQueue
 }
+
+// OpenPoll ...
+func OpenPoll() *Poll {
+
+	l := new(Poll)
+	p, err := syscall.EpollCreate1(0)
+	if err != nil {
+		panic(err)
+	}
+	l.fd = p
+
+	r0, _, e0 := syscall.Syscall(syscall.SYS_EVENTFD2, 0, 0, 0)
+	if e0 != 0 {
+		syscall.Close(p)
+		panic(err)
+	}
+	l.wfd = int(r0)
+	l.AddRead(l.wfd)
+	return l
+}
