696defa8187e5ebdf0b66c5912d7fcb1b6effac0
GarryChen <chenymj23@gmail.com>
2022-07-01 16:54:32 +0800

    http2 flow control init

--- http2/http2.go
+++ http2/http2.go
@@ -19,3 +19,24 @@ const (
 	ErrCodeInadequateSecurity ErrCode = 0xc
 	ErrCodeHTTP11Required     ErrCode = 0xd
 )
+
+const (
+	maxConcurrentStreams = 1<<31 - 1
+	maxInitialWindowSize = 1<<31 - 1
+)
+
+// FrameType represents Frame Type Registry, defined in RFC 7540 section 11.2.
+type FrameType uint8
+
+const (
+	FrameData         FrameType = 0x0
+	FrameHeaders      FrameType = 0x1
+	FramePriority     FrameType = 0x2
+	FrameRSTStream    FrameType = 0x3
+	FrameSettings     FrameType = 0x4
+	FramePushPromise  FrameType = 0x5
+	FramePing         FrameType = 0x6
+	FrameGoAway       FrameType = 0x7
+	FrameWindowUpdate FrameType = 0x8
+	FrameContinuation FrameType = 0x9
+)
--- http2/stream.go
+++ http2/stream.go
@@ -12,3 +12,15 @@ const (
 	StateHalfClosedRemote
 	StateClosed
 )
+
+type stream struct {
+	id    uint32
+	state StreamState
+
+	// 5.3.1
+	weight   uint8
+	parent   *stream
+	children map[uint32]*stream
+
+	recvFlow *flowController
+}
