4c3c9875e058777cdbac2b18c51fb64252e379a9
GarryChen <chenymj23@gmail.com>
2022-10-16 20:19:22 +0800

    concurrent map test

--- concurrent_map/concurrent_map_test.go
+++ concurrent_map/concurrent_map_test.go
@@ -57,3 +57,133 @@ func TestGet(t *testing.T) {
 		t.Error("item was modified.")
 	}
 }
+
+func TestHas(t *testing.T) {
+	m := New[Animal]()
+
+	// Get a missing element
+	if m.Has("Money") == true {
+		t.Error("element shouldn't exists.")
+	}
+
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	if m.Has("elephant") == false {
+		t.Error("element exists, expecting Has to return True.")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	m := New[Animal]()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+
+	m.Remove("monkey")
+
+	if m.Count() != 0 {
+		t.Error("Expecting count to be zero once item was removed.")
+	}
+
+	temp, ok := m.Get("monkey")
+
+	if ok != false {
+		t.Error("Expecting ok to be false for missing items.")
+	}
+
+	if (temp != Animal{}) {
+		t.Error("Expecting item to be nil after its removal.")
+	}
+
+	// Remove a none existing element.
+	m.Remove("noone")
+}
+
+func TestRemoveCb(t *testing.T) {
+	m := New[Animal]()
+
+	monkey := Animal{"monkey"}
+	m.Set("monkey", monkey)
+	elephant := Animal{"elephant"}
+	m.Set("elephant", elephant)
+
+	var (
+		mapKey   string
+		mapVal   Animal
+		wasFound bool
+	)
+	cb := func(key string, val Animal, exists bool) bool {
+		mapKey = key
+		mapVal = val
+		wasFound = exists
+
+		return val.name == "monkey"
+	}
+
+	// Monkey should be removed
+	result := m.RemoveCb("monkey", cb)
+	if !result {
+		t.Errorf("Result was not true")
+	}
+
+	if mapKey != "monkey" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if mapVal != monkey {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if !wasFound {
+		t.Errorf("Key was not found")
+	}
+
+	if m.Has("monkey") {
+		t.Errorf("Key was not removed")
+	}
+
+	// Elephant should not be removed
+	result = m.RemoveCb("elephant", cb)
+	if result {
+		t.Errorf("Result was true")
+	}
+
+	if mapKey != "elephant" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if mapVal != elephant {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if !wasFound {
+		t.Errorf("Key was not found")
+	}
+
+	if !m.Has("elephant") {
+		t.Errorf("Key was removed")
+	}
+
+	// Unset key should remain unset
+	result = m.RemoveCb("horse", cb)
+	if result {
+		t.Errorf("Result was true")
+	}
+
+	if mapKey != "horse" {
+		t.Error("Wrong key was provided to the callback")
+	}
+
+	if (mapVal != Animal{}) {
+		t.Errorf("Wrong value was provided to the value")
+	}
+
+	if wasFound {
+		t.Errorf("Key was found")
+	}
+
+	if m.Has("horse") {
+		t.Errorf("Key was created")
+	}
+}
